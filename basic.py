fbc1fcd48f5fb95ea1d8cbe9596e6abb5d4f37c6
banzai100 <banzai100@gmail.com>
2024-01-06 01:54:39 +0500

    first case solved (no logging)

--- basic.py
+++ basic.py
@@ -3,13 +3,17 @@ from selenium.webdriver.support import expected_conditions as ec
 
 
 class BasePage:
+
     def __init__(self, driver):
         self.driver = driver
-        self.base_url = "https://sbis.ru/"
 
-    def find_element(self, locator, time=5):
+    def find_element(self, locator, time=10):
         return WebDriverWait(self.driver, time).until(ec.presence_of_element_located(locator),
-                                                      message=f"Отсутствует элемент {locator}")
+                                                      message=f"Can't find element by locator {locator}")
+
+    def find_elements(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(ec.presence_of_all_elements_located(locator),
+                                                      message=f"Can't find elements by locator {locator}")
 
     def wait_element(self, locator, time=5):
         return WebDriverWait(self.driver, time).until(ec.invisibility_of_element_located(locator))
--- test_sbis.py
+++ test_sbis.py
@@ -1,15 +1,21 @@
 from SbisPages import SbisSearchHelper
 from TenzorPages import TenzorSearchHelper
-from time import sleep
+
+
+# import logging
+
+# logging.basicConfig(level=logging.INFO, filename="test.log", encoding="utf-8", filemode="w")
 
 
 def test_sbis_contacts(browser):
     sbis_main_page = SbisSearchHelper(browser)
     sbis_main_page.go_to_url()
-    sbis_main_page.click_contact_button()
-    sbis_main_page.click_tenzor_button()
+    sbis_main_page.click_button("SBIS_CONTACTS")
+    sbis_main_page.click_button("SBIS_TENSOR_BANNER", wait=True)
     sbis_main_page.driver.switch_to.window(sbis_main_page.driver.window_handles[1])
-    tenzor_main_page = TenzorSearchHelper(browser)
-    tenzor_main_page.find_element_power()
-    sleep(5)
-
+    tensor_main_page = TenzorSearchHelper(browser)
+    tensor_main_page.find_element(tensor_main_page.LOCATORS["TENSOR_POWER_IN_PEOPLE"])
+    tensor_main_page.click_button("TENSOR_ABOUT", wait=True)
+    assert tensor_main_page.driver.current_url == "https://tensor.ru/about"
+    tensor_main_page.find_element(tensor_main_page.LOCATORS["TENSOR_BLOCK3"])
+    assert tensor_main_page.check_images("TENSOR_BLOCK3_IMAGES") == True
